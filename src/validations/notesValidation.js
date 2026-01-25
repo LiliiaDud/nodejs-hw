714167ef3ca5ad0965a151b4261c6f0701558c14
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-01-25 21:07:09 +0100

    removed .max() for the search field in getAllNotesSchema, and title,content  field  in  createNoteSchema and updateNoteSchema

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -11,7 +11,7 @@ export const getAllNotesSchema = {
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
-    search: Joi.string().allow('').max(100),
+    search: Joi.string().allow(''),
   }),
 };
 
@@ -23,22 +23,11 @@ export const noteIdSchema = {
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).max(30).required().messages({
-      'string.base': 'Title must be a string',
-      'string.min': 'Title should have at least {#limit} characters',
-      'string.max': 'Title should have at most {#limit} characters',
-      'any.required': 'Title is required',
-    }),
-    content: Joi.string().allow('').max(1000).messages({
-      'string.max': 'Content should have at most {#limit} characters',
-    }),
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(''),
     tag: Joi.string()
-      .valid(...TAGS)
-      .messages({
-        'any.only':
-          'Tag must be one of: Work, Personal, Meeting, Shopping, Ideas, Travel, Finance, Health, Important, Todo',
-      }),
-  }).unknown(false),
+      .valid(...TAGS),
+  }),
 };
 
 export const updateNoteSchema = {
@@ -46,22 +35,9 @@ export const updateNoteSchema = {
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).max(30).messages({
-      'string.base': 'Title must be a string',
-      'string.min': 'Title should have at least {#limit} characters',
-      'string.max': 'Title should have at most {#limit} characters',
-    }),
-    content: Joi.string().allow('').max(1000),
-    tag: Joi.string()
-      .valid(...TAGS)
-      .messages({
-        'any.only':
-          'Tag must be one of: Work, Personal, Meeting, Shopping, Ideas, Travel, Finance, Health, Important, Todo',
-      }),
-  })
-    .min(1)
-    .messages({
-      'object.min': 'At least one field must be provided for update',
-    })
-    .unknown(false),
+    title: Joi.string().min(1),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+    }).min(1)
 };
+ 
