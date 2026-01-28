418f0db89aebc9b6c8b8acdff52ff21cb20e1a0d
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-01-28 13:06:21 +0100

    Create a User model with the following properties: username, email, password.

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+//Hook pre('save') to set the username to be the same as the email by default when creating a user.
+userSchema.pre('save', function (next) {
+  if (!this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+//remove the password from the user object before sending the response
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+  delete userObject.password;
+  return userObject;
+};
+
+export const User = model('User', userSchema);
