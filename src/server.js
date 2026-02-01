0dc00f2c91e7ef7457ab2c9fb011326a1054d2fb
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-01 14:15:36 +0100

    remove unnecessary code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
@@ -8,7 +9,6 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -17,19 +17,19 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
+// Auth routers
 app.use(authRoutes);
+// Notes routers
 app.use(notesRoutes);
-
+// Middleware 404
 app.use(notFoundHandler);
+// Celebrate errors
 app.use(errors());
+// Middleware Errors
 app.use(errorHandler);
-
+// Connect MongoDB
 await connectMongoDB();
-
+// Launch Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
