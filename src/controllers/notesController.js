3cc79e5d3f0ff8686be4e85d7d474cad4cc63e82
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-01 13:43:32 +0100

    replace the style - instead of "next(err)" with "throw"

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,93 +1,88 @@
 import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
-export const getAllNotes = async (req, res, next) => {
-  try {
-    const { page = 1, perPage = 10, tag, search } = req.query;
-    const skip = (page - 1) * perPage;
+export const getAllNotes = async (req, res) => {
+  const { page = 1, perPage = 10, tag, search } = req.query;
+  const skip = (page - 1) * perPage;
+  const notesQuery = Note.find({ userId: req.user._id });
 
-    const filter = {};
-    if (tag) filter.tag = tag;
-    if (search && search.trim() !== '') filter.$text = { $search: search };
+  if (tag) {
+    notesQuery.where('tag').equals(tag);
+  }
 
-    const notesQuery = Note.find(filter);
-    const [totalNotes, notes] = await Promise.all([
-      notesQuery.clone().countDocuments(),
-      notesQuery.skip(skip).limit(perPage),
-    ]);
-
-    const totalPages = Math.ceil(totalNotes / perPage);
-
-    res.status(200).json({
-      page,
-      perPage,
-      totalPages,
-      totalNotes,
-      notes,
+  if (search) {
+    notesQuery.where({
+      $text: {
+        $search: search,
+      },
     });
-  } catch (err) {
-    next(err);
   }
+
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalPages,
+    totalNotes,
+    notes,
+  });
 };
 
-export const getNoteById = async (req, res, next) => {
-  try {
-    const { noteId } = req.params;
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
 
-    const note = await Note.findById(noteId);
-    if (!note) {
-      return next(createHttpError(404, 'Note not found'));
-    }
-    res.status(200).json(note);
-  } catch (err) {
-    if (err.name === 'CastError') {
-      return next(createHttpError(404, 'Note not found'));
-    }
-    next(err);
+  const note = await Note.findOne({
+    _id: noteId,
+    userId: req.user._id,
+  });
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
   }
+  res.status(200).json(note);
 };
 
-export const createNote = async (req, res, next) => {
-  try {
-    const note = await Note.create(req.body);
-    res.status(201).json(note);
-  } catch (err) {
-    next(err);
-  }
+export const createNote = async (req, res) => {
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id,
+  });
+  res.status(201).json(note);
 };
 
-export const deleteNote = async (req, res, next) => {
-  try {
-    const { noteId } = req.params;
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
 
-    const note = await Note.findOneAndDelete({ _id: noteId });
-    if (!note) {
-      return next(createHttpError(404, 'Note not found'));
-    }
-    res.status(200).json(note);
-  } catch (err) {
-    if (err.name === 'CastError') {
-      return next(createHttpError(404, 'Note not found'));
-    }
-    next(err);
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+    userId: req.user._id,
+  });
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
   }
+  res.status(200).json(note);
 };
 
-export const updateNote = async (req, res, next) => {
-  try {
-    const { noteId } = req.params;
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
 
-    const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+  const note = await Note.findOneAndUpdate(
+    {
+      _id: noteId,
+      userId: req.user._id,
+    },
+    req.body,
+    {
       new: true,
-    });
-    if (!note) {
-      return next(createHttpError(404, 'Note not found'));
-    }
-    res.status(200).json(note);
-  } catch (err) {
-    if (err.name === 'CastError') {
-      return next(createHttpError(404, 'Note not found'));
-    }
-    next(err);
+    },
+  );
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
   }
+  res.status(200).json(note);
 };
