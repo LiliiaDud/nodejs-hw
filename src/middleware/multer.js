8d56d038703d9ad3dd4c40515719f70a743bb696
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-06 17:22:25 +0100

    change mimetype

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -5,19 +5,10 @@ export const upload = multer({
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
-  fileFilter: (req, file, callback) => {
-    const fileTypes = ['image/jpeg', 'image/png'];
-
-    if (fileTypes.includes(file.mimetype)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Only images allowed'));
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype || !file.mimetype.startsWith('image/')) {
+      return cb(new Error('Only images allowed'));
     }
+    cb(null, true);
   },
-  // fileFilter: (req, file, cb) => {
-  //   if (!file.mimetype || !file.mimetype.startsWith('image/')) {
-  //     return cb(new Error('Only images allowed'));
-  //   }
-  //   cb(null, true);
-  // },
 });
