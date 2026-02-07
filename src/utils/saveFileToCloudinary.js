cc128a0ab0b4359c4ac99c07c4653037d3a94298
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-07 19:31:32 +0100

    add redmi, .env.example. change saveFileToCloudinary

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -8,16 +8,16 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const saveFileToCloudinary = async (buffer) => {
+export const saveFileToCloudinary = async (buffer, userId) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder: 'notes-app/avatars',
         resource_type: 'image',
-        unique_filename: true,
-        use_filename: false,
-        //public_id: `avatar-${userId}`, //назва мого файлу, яка буде зберігатися в Cloudinary. Вона складається з префікса "avatar-" та унікального ідентифікатора користувача (userId). Це дозволяє легко ідентифікувати та керувати файлами, пов'язаними з конкретними користувачами.
+        //   use_filename: false,
+        public_id: `avatar-${userId}`, //назва мого файлу, яка буде зберігатися в Cloudinary. Вона складається з префікса "avatar-" та унікального ідентифікатора користувача (userId). Це дозволяє легко ідентифікувати та керувати файлами, пов'язаними з конкретними користувачами.
         overwrite: true,
+        unique_filename: false,
       },
       (err, result) => (err ? reject(err) : resolve(result)),
     );
